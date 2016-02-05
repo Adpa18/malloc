2f5f1b511fc18cf24f4757cdc7ef34ff6b7d7e63
Nicolas Constanty <nicolas.constanty@epitech.eu>
2016-02-05 16:20:26 +0100

    main

--- test/main.c
+++ test/main.c
@@ -13,6 +13,30 @@
 
 int main()
 {
-    printf("%p\n", malloc(10));
-    return (0);
+  char *toto;
+  int	*nbs;
+  int i = 0;
+  
+  toto = (char *)malloc(sizeof(char) * 4);
+  toto[0] = 'h';
+  toto[1] = 'e';
+  toto[2] = 'y';
+  toto[3] = 0;
+  printf("%s\n", toto);
+
+  nbs = (int *)malloc(sizeof(int) * 3);
+  nbs[0] = 0;
+  nbs[1] = 1;
+  nbs[2] = 2;
+
+  printf("%d\n%d\n%d\n", nbs[0], nbs[1], nbs[2]);
+
+  while (i < 10)
+    {
+      malloc(1);
+      ++i;
+    }
+  free(toto);
+  printf("%s\n", toto);
+  return (0);
 }
